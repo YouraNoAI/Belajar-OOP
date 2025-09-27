446d91031734f97ae69e899d16446ed78235bf88
YouraNoAI <dhoyoxofficial23@gmail.com>
2025-09-27 14:49:29 +0700

    week4

--- week4/this.php
+++ week4/this.php
@@ -4,22 +4,50 @@ class Car {
 
     public function IsiBensin($liter) {
         $this->Tanki += $liter;
+        return $this;
     }
-
     public function Mengendarai ($jarak){
         $this->Tanki -= $jarak / 80;
-        if ($this->Tanki < 0) {
-            $this->Tanki = "Mogok Bre";
+        if ($this->Tanki == 0) {
+            $this->Tanki = "Hoki 100 Tahun sekali ke pake, pas sampe langsung habis";
         }
-        if ($this->Tanki > 0) {
-            $this->Tanki = $this->Tanki;
+        elseif ($this->Tanki < 0) {
+            $this->Tanki = "Mogok Bre, ngutang tadi di POM";
         }
+        elseif ($this->Tanki > 0) {
+            $this->Tanki = $this->Tanki . " Liter";
+        }
+        return $this;
+    }
+    }
+
+class Motor {
+    public $Tanki = 0 ;
+    public function IsiBensin($liter) {
+        $this->Tanki += $liter;
+        return $this;
+    }
+    public function Mengendarai ($jarak){
+        $this->Tanki -= $jarak / 50;
+        if ($this->Tanki == 0) {
+            $this->Tanki = "Hoki 100 Tahun sekali ke pake, pas sampe langsung habis";
+        }
+        elseif ($this->Tanki < 0) {
+            $this->Tanki = "Mogok Bre, ngutang tadi di POM";
+        }
+        elseif ($this->Tanki > 0) {
+            $this->Tanki = $this->Tanki . " Liter";
+        }
+        return $this;
     }
 }
 
-$car = new Car();
-$car->IsiBensin(10);
-$car->Mengendarai(1000);
-echo "Sisa Bensin : " . $car->Tanki . "";
+$car1 = new Car();
+$sisa = $car1->IsiBensin(15)->Mengendarai(400)->Tanki;
+echo "Sisa Bensin Mobil: " . $sisa . "\n";
+
+$motor = new Motor();
+$sisa = $motor->IsiBensin(2)->Mengendarai(120)->Tanki;
+echo "Sisa Bensin Motor: " . $sisa . "\n"
 
 ?>
